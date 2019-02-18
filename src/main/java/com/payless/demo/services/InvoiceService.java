9d2ba99f93235ea6f90f70d180b74545b1b70a41
root <root@root-PC>
2019-02-18 00:34:36 -0300

    18.01.2019 terminado el invoiceService para trader

--- src/main/java/com/payless/demo/services/InvoiceService.java
+++ src/main/java/com/payless/demo/services/InvoiceService.java
@@ -28,7 +28,7 @@ import com.payless.demo.repositories.TraderRepository;
  * -REMOVE INVOICE IN A TRADER
  * -DELETE ALL INVOICE IN A TRADER
  * -ADD PRODUCTS IN A INVOICE OD A TRADER
- * - DELETE A PRODUCT AN INVOICE
+ * -DELETE A PRODUCT AN INVOICE
  * -UPDATE A QUANTITY IN A INVOICEPRODUCT
  * */
 
@@ -49,7 +49,7 @@ public class InvoiceService {
 
 
 
-	@PostMapping(path="/invoice/addinvoice/{id}")
+	@PostMapping(path="/invoice/addinvoice/trader{id}")
 	public Invoice addInvoice(@PathVariable("id") long id){
 		Trader traderdb = traderRepository.findById(id).get();
 		Invoice invoice= new  Invoice();
@@ -72,41 +72,59 @@ public class InvoiceService {
 	}
 
 
+	public boolean isIdInListInvoices(List<Invoice> list, long id){
+		boolean flag=false;
+		for (int i=0 ; i<list.size() ; i++ ){
+			if(list.get(i).getId() == id){
+				flag=true;
+				break;
+			}
+		}	
+
+		return flag;
+	}
+
+
+
 	@PostMapping(path="/invoice/addproduct/trader/{id}/invoice/{idinvoice}/prod/{prod}/cantidad/{canti}")
-	public boolean addProductInvoice(@PathVariable("id") long id, @PathVariable("idinvoice") long idinvoice ,@PathVariable("prod") long idprod, @PathVariable("canti") int cantidad){
+	public boolean addProductInvoice(@PathVariable("id") long id, @PathVariable("idinvoice") long idinvoice ,
+			@PathVariable("prod") long idprod, @PathVariable("canti") int cantidad){
 
 		boolean flag=false;
 
-		if(invoiceRepository.existsById(idinvoice)){
-
+		if(cantidad!=0) {
 			Trader traderdb = traderRepository.findById(id).get();
-			Invoice dbinvoice = invoiceRepository.findById(idinvoice).get();
-			dbinvoice.setTrader(traderdb);
-			Product dbproduct = productRepository.findById(idprod).get();
-			dbinvoice.addInvoiceProduct(dbproduct, cantidad);				
-			invoiceRepository.save(dbinvoice);
-			flag=true;
+			List<Invoice> listInvoices = (List<Invoice>) traderdb.getInvoice();
 
+			if(isIdInListInvoices(listInvoices, idinvoice) == true){
+				Invoice dbinvoice = invoiceRepository.findById(idinvoice).get();
+				dbinvoice.setTrader(traderdb);
+				Product dbproduct = productRepository.findById(idprod).get();
+				dbinvoice.addInvoiceProduct(dbproduct, cantidad);				
+				invoiceRepository.save(dbinvoice);
+				flag=true;
+			}
 		}
+
 		return flag;
 
 	}
 
 	@DeleteMapping(path="/invoice/deleteinvoice/{idinvoice}/product/{idprod}")
 	public boolean removeProductInvoice(@PathVariable("id") long id, @PathVariable("idprod") long idprod){
-		
+
 		boolean flag=false;
 		Collection<InvoiceProduct>  invoiceProducDB;
 
 		if(invoiceRepository.existsById(id)){
-			
+
 			Product pr =  productRepository.findById(idprod).get();
 			Invoice invoicedb = invoiceRepository.findById(id).get();
 			invoiceProducDB = invoicedb.getProducts();
 			invoiceProducDB.removeIf((InvoiceProduct ip) -> ip.getPoduct().equals(pr));
 			invoiceRepository.save(invoicedb);
 			flag=true;
-	
+
 		}
 
 
@@ -116,35 +134,41 @@ public class InvoiceService {
 
 
 
-	@PutMapping("/invoice/updateinvoice/{id}/product/{idprod}/cantidad/{cant}")
-	public boolean updateQuantityProduct(@PathVariable("id") long id ,@PathVariable("idprod") long prod, @PathVariable("cant") int cantidad){
-	
-		InvoiceProduct  invoiceProducDB;
+	@PostMapping(path="/invoice/udproduct/trader/{id}/invoice/{idinvoice}/prod/{prod}/cantidad/{canti}")
+	public boolean updateQuantityProduct(@PathVariable("id") long id, @PathVariable("idinvoice") long idinvoice ,
+			@PathVariable("prod") long idprod, @PathVariable("canti") int cantidad){
+
 
 		boolean flag= false;
 
 		if(cantidad!=0) {
+
+			Trader traderdb = traderRepository.findById(id).get();
+			List<Invoice> listInvoices = (List<Invoice>) traderdb.getInvoice();
+
+			if(isIdInListInvoices(listInvoices, idinvoice) == true){
+
+					Invoice invoidedb  = invoiceRepository.findById(idinvoice).get();
+					List<InvoiceProduct>  invoiceProducDB = (List<InvoiceProduct>) invoidedb.getProducts();
+					for(int i=0; i< invoiceProducDB.size();i++){
+						if(invoiceProducDB.get(i).getPoduct().getId() == idprod){
+					
+							invoiceProducDB.get(i).setQuantity((int)cantidad);
+							invoiceRepository.save(invoidedb);
+							flag=true;
+							break;
+						}	
+					}
 			
-			if(productRepository.existsById(prod)){
-				Invoice invoidedb  = invoiceRepository.findById(id).get();
-				int size = invoidedb.getProducts().size();
-				
-				for(int i=0; i< size;i++){
-					/*if(stockProducts.get(i).getProduct().getId()==idprod){
-						stockProducts.get(i).setQuantity(cantidad);
-						flag=true;
-						traderRepository.save(traderdb);
-					}	*/
-				}
 			}
 
-			
+
 		}
 
 		return flag;
 	}
 
-	
+
 
 
 
