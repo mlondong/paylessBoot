4e4fc9d11dc42da02ec0bb6d06782e10a5f311c0
mlondong <mauricio.londono@gmail.com>
2019-05-06 23:26:11 -0300

    06/05/2019

--- src/main/java/com/payless/demo/controller/ConsumerController.java
+++ src/main/java/com/payless/demo/controller/ConsumerController.java
@@ -13,6 +13,8 @@ import javax.validation.Valid;
 
 import org.apache.catalina.Session;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -21,12 +23,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.payless.demo.model.Address;
 import com.payless.demo.model.Consumer;
 import com.payless.demo.model.Product;
 import com.payless.demo.model.Stock;
@@ -35,7 +39,8 @@ import com.payless.demo.model.Trader;
 import com.payless.demo.services.ConsumerServiceImp;
 import com.payless.demo.services.ProductServiceImp;
 import com.payless.demo.services.TraderServiceImp;
-import com.payless.demo.util.UtilOperations;
+import com.payless.demo.util.Passgenerator;
+
 
 
 @Controller
@@ -108,13 +113,90 @@ public class ConsumerController {
 	}
 
 	
-	/*@RequestMapping(path="/consumer/getdescription", method={RequestMethod.POST, RequestMethod.GET}, produces = "application/json")
-	@ResponseBody
-	public List<Product> getProductName(@RequestParam("description") String description){
-		return productServiceImp.findByContainDescription(description);
-	} */
+	@GetMapping("/consumer/viewprofile")
+	public ModelAndView viewProfile() {
+		ModelAndView modelAndView = new ModelAndView("c_profile");
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Consumer consumer = consumerServiceImp.queryFindByUserName(auth.getName());
+        modelAndView.addObject("consumer", consumer);
+        return modelAndView;
+	}
+	
 
+	@RequestMapping(path="/consumer/upgeneral",  method={RequestMethod.POST, RequestMethod.GET})
+	public ModelAndView  updatingConsumer(@RequestParam("firstName") String firstName, 
+										  @RequestParam("lastName") String lastName, @RequestParam("dni") Long dni ){
 
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    Consumer consumer = consumerServiceImp.queryFindByUserName(auth.getName());
+	    ModelAndView modelAndView = new ModelAndView("c_profile");
+		modelAndView.addObject("consumer", consumer);
+	   	
+		try{
+			   consumer.setFirstName(firstName);
+			   consumer.setLastName(lastName);
+			   consumer.setName(consumer.getName());
+			   consumer.setDni(dni);
+			   consumerServiceImp.save(consumer);
+			   modelAndView.addObject("Update", "Update it !"); 
+		}catch(Exception e){
+			modelAndView.addObject("Error", "Error in data Information...");
+		}
+	
+		return modelAndView;
+	}
+			
 
+	@RequestMapping(path="/consumer/uplogin",  method={RequestMethod.POST, RequestMethod.GET})
+	public ModelAndView  updatingConsumerLogin(@RequestParam("username") String username, 
+										       @RequestParam("pass") String pass){
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    Consumer consumer = consumerServiceImp.queryFindByUserName(auth.getName());
+	    ModelAndView modelAndView = new ModelAndView("c_profile");
+		modelAndView.addObject("consumer", consumer);
+	   	System.out.println(consumer);
+		
+		try{
+				Passgenerator encoder = new Passgenerator(4);
+				String newpass= encoder.generate(pass);
+			    consumer.setName(username);
+			    consumer.setPassword(newpass);
+			    consumerServiceImp.save(consumer);
+			   modelAndView.addObject("UpdateLogin", "Update it !"); 
+		}catch(Exception e){
+			modelAndView.addObject("ErrorLogin", "Error in data Information ...");
+		}
+	
+		return modelAndView;
+	}
+			
+
+	
+	@RequestMapping(path="/consumer/upaddress",  method={RequestMethod.POST, RequestMethod.GET})
+	public ModelAndView  updatingConsumerAddress(@RequestParam("description") String description,
+											     @RequestParam("city") int city,
+										         @RequestParam("zona") int zona){
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    Consumer consumer = consumerServiceImp.queryFindByUserName(auth.getName());
+	    ModelAndView modelAndView = new ModelAndView("c_profile");
+		modelAndView.addObject("consumer", consumer);
+	   	System.out.println(consumer);
+		
+		try{
+				Address address = new Address(description, city, zona);
+				consumer.setAddress(address);;
+			    consumerServiceImp.save(consumer);
+				modelAndView.addObject("UpdateAddress", "Update it !"); 
+		}catch(Exception e){
+			modelAndView.addObject("ErrorAddress", "Error in data Information ...");
+		}
+	
+		return modelAndView;
+	}
+
+	
+	
 	
 }
